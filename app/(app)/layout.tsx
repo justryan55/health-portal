1e42744fd2fc334dff161f66e7d471aa8af41381
Ryan Irani <ryan.irani@gmail.com>
2025-07-17 14:53:51 +0200

    fix: revert back to previous - readded html/body

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -32,31 +32,35 @@ export default async function RootLayout({
   }
 
   return (
-    <SupabaseProvider>
-      <DateProvider>
-        <UserProvider>
-          <ProfileProvider>
-            <WorkoutProvider>
-              <SidebarProvider
-                style={
-                  {
-                    "--sidebar-width": "calc(var(--spacing) * 72)",
-                    "--header-height": "calc(var(--spacing) * 12)",
-                  } as React.CSSProperties
-                }
-              >
-                <AppSidebar variant="inset" />
-                <SidebarInset>
-                  <SiteHeader />
+    <html lang="en">
+      <body>
+        <SupabaseProvider>
+          <DateProvider>
+            <UserProvider>
+              <ProfileProvider>
+                <WorkoutProvider>
+                  <SidebarProvider
+                    style={
+                      {
+                        "--sidebar-width": "calc(var(--spacing) * 72)",
+                        "--header-height": "calc(var(--spacing) * 12)",
+                      } as React.CSSProperties
+                    }
+                  >
+                    <AppSidebar variant="inset" />
+                    <SidebarInset>
+                      <SiteHeader />
 
-                  {children}
-                  <Toaster />
-                </SidebarInset>
-              </SidebarProvider>
-            </WorkoutProvider>
-          </ProfileProvider>
-        </UserProvider>
-      </DateProvider>
-    </SupabaseProvider>
+                      {children}
+                      <Toaster />
+                    </SidebarInset>
+                  </SidebarProvider>
+                </WorkoutProvider>
+              </ProfileProvider>
+            </UserProvider>
+          </DateProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
   );
 }
